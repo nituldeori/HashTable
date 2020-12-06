265de5bba09b11870caa956ce189303040b56c01
unknown <nituldeori63@gmail.com>
2020-12-07 00:55:21 +0530

    Added: Ability to create a simple BST of three nodes

---
+++ MyBinaryTreeTest.java
@@ -0,0 +1,19 @@
+package com.datastructuredemo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyBinaryTreeTest {
+    @Test
+    public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(3, size);
+    }
+
+}
